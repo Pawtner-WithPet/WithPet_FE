10d89fd74992908249f737a3ad6f9172c7031d62
Ninanyu23 <Ninanyu0704@gmail.com>
2025-06-29 21:27:38 +0900

    Revert "Final sprint1"

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,4 @@
-
-
-
-
-{
-  "compilerOptions": {
-    "target": "esnext",
-    "module": "esnext",
-    "moduleResolution": "node",
-    "jsx": "react",
-    "allowJs": true,
-    "skipLibCheck": true,
-    "esModuleInterop": true,
-    "strict": true,
-    "forceConsistentCasingInFileNames": true,
-    "noEmit": true,
-    "isolatedModules": true
-  },
-  "exclude": [
-    "node_modules",
-    "babel.config.js",
-    "metro.config.js",
-    "jest.config.js"
-  ]
-}
-
-/*module.exports = {
+module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
 };
-*/
--- src/constants/colors.ts
+++
@@ -1,7 +0,0 @@
-// 전역 컬러 정의
-export const Colors = {
-  background: '#F9FAFD',
-  card: '#FFFFFF',
-  primaryLight: '#E5E3FF',
-  text: '#333333',
-};
