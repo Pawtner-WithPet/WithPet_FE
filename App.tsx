f1068088cbb0cf67b4e406f71830d7a65bd5d7f6
seojin122 <akflsdlek23@naver.com>
2025-07-02 14:17:01 +0900

    ✨ 비문 촬영 페이지
    비문 촬영 페이지

--- App.tsx
+++ App.tsx
@@ -6,12 +6,21 @@ import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from './src/navigation/TabNavigator';
 import { Colors } from './src/constants/colors';
+import NoseCamera from './src/screens/Home/Nose/NoseCamera';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => (
   <NavigationContainer>
     <SafeAreaView style={styles.safe}>
       <StatusBar barStyle="dark-content" backgroundColor={Colors.primaryLight} />
-      <TabNavigator />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* ✅ 탭 전체를 Stack의 하나의 화면으로 넣음 */}
+        <Stack.Screen name="MainTabs" component={TabNavigator} />
+        {/* ✅ NoseCamera도 Stack에 등록 */}
+        <Stack.Screen name="NoseCamera" component={NoseCamera} />
+      </Stack.Navigator>
     </SafeAreaView>
   </NavigationContainer>
 );
